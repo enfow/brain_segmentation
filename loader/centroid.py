06cb94589e187ddf8780d0bcf79a1b4c867290b5
enfow <wgm0601@gmail.com>
2020-05-05 23:58:14 +0900

    fix centroid update function

--- loader/centroid.py
+++ loader/centroid.py
@@ -40,3 +40,11 @@ def get_centroid_list(label, present_label_list):
         centroid_list.append(get_centroid_obj(label[idx], present_label_list))
     return centroid_list
 
+def get_updated_centroid_list(updated_label, centroid_list):
+    updated_centroid_list = centroid_list
+    num_of_brain = updated_label.shape[0]
+    for idx in range(num_of_brain):
+        temp = updated_label[idx].nonzero()
+        vxs = np.asarray(temp).T
+        updated_centroid_list[idx].update_barycentres(vxs, updated_label[idx][temp])
+    return updated_centroid_list
